1d0c61a6068c1b5feb30a0111b6596f3bca82ecd
Sandra Rosch <sandra.rosch@uzh.ch>
2022-04-11 10:52:25 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "./Button";
+import Stomp from "stompjs";
+//import socket from "socketjs";
+import SockJS from "sockjs-client";
+
+
+
+//var url = "ws://localhost:15674/ws";
+//var client = Stomp.client(url);
+var stompClient = null;
+
+
+var socket1 = new SockJS('http://localhost:8082/gs-guide-websocket');
+stompClient = Stomp.over(socket1);
+stompClient.connect();
+stompClient.subscribe('/topic/greetings', function (greeting) {
+    showGreeting(JSON.parse(greeting.body).content);
+});
+
+function showGreeting(message) {
+    console.log("show greeting" +message);
+}
+
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify("name sent"));
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+
+                <Button
+                    width="100%"
+                    onClick={() => sendName()}
+                >
+                    Click here to register
+                </Button>
+
+
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
+
